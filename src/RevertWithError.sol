f1fb5b0c14fbf99ba2b1b215324f019372d0fec5
Oke.A <Okea.dev@gmail.com>
2025-12-26 18:57:12 +0100

    fix: updated RevertWithError puzzle

--- src/RevertWithError.sol
+++ src/RevertWithError.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.8.13;
 
 contract RevertWithError {
     function main() external pure {
+        bytes32 selector = bytes32(abi.encodeWithSignature("Error(string)","RevertRevert"));
         assembly {
             // revert the function with an error of type `Error(string)`
             // use "RevertRevert" as error message
             // Hint: The error type is a predefined four bytes. See https://www.rareskills.io/post/try-catch-solidity
+            mstore(0x00, selector)
+            mstore(0x04, 0x20) // Store offset to string (32)
+            mstore(0x24, 0x0c) // Store length of string (12)
+            mstore(0x44, 0x5265766572745265766572740000000000000000000000000000000000000000) // Store "RevertRevert" padded
+            revert(0x00, 0x64) // Revert with 100 bytes
         }
     }
 }
